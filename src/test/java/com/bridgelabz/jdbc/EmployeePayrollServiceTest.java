213f54fdde33d0773ba070bf7ba9d2480b7656da
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-07 00:38:25 +0530

    added: retrieve data from payroll_service database

---
+++ src/test/java/com/bridgelabz/jdbc/EmployeePayrollServiceTest.java
@@ -0,0 +1,21 @@
+package com.bridgelabz.jdbc;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import exception.DatabaseConnectionException;
+import model.EmployeePayrollData;
+import service.EmployeePayrollService;
+
+public class EmployeePayrollServiceTest {
+
+	 @Test
+	    public void givenEmployeePayrollInDB_WhenDataRetrieved_ShouldMatchEmployeeCount() throws DatabaseConnectionException{
+	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+	        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData();
+	        Assert.assertEquals(4, employeePayrollData.size());
+	    }
+
+}
