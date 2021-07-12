4681124d549b82af92899f79310a8d9c8abf3bdf
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-12 06:50:15 +0530

    added: documentation to test cases

--- src/test/java/com/bridgelabz/jdbc/EmployeePayrollServiceTest.java
+++ src/test/java/com/bridgelabz/jdbc/EmployeePayrollServiceTest.java
@@ -1,3 +1,10 @@
+/********************************************************************************************
+ * @Purpose : Test Cases for Employee Payroll Database Service program to 
+ * perform various operations in database. 
+ * @author Rosy Rupali
+ * @version 1.0
+ * @since 07-07-2021
+ ************************************************************************************************/
 package com.bridgelabz.jdbc;
 
 import java.time.LocalDate;
@@ -13,14 +20,16 @@ import service.EmployeePayrollService;
 
 public class EmployeePayrollServiceTest {
 
+	// UC2
 	@Test
 	public void givenEmployeePayrollInDB_WhenDataRetrieved_ShouldMatchEmployeeCount()
 			throws DatabaseConnectionException {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData();
-		Assert.assertEquals(4, employeePayrollData.size());
+		Assert.assertEquals(3, employeePayrollData.size());
 	}
 
+	// UC3
 	@Test
 	public void givenEmployeePayrollInDB_WhenUpdated_ShouldSyncWithDB() throws DatabaseConnectionException {
 		List<EmployeePayrollData> employeePayrollData;
@@ -31,6 +40,19 @@ public class EmployeePayrollServiceTest {
 		Assert.assertTrue(result);
 	}
 
+	// UC4
+	@Test
+	public void givenEmployeePayrollInDB_WhenUpdatedUsingPreparedStatement_ShouldSyncWithDB()
+			throws DatabaseConnectionException {
+		List<EmployeePayrollData> employeePayrollData;
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		employeePayrollData = employeePayrollService.readEmployeePayrollData();
+		employeePayrollService.updateEmployeeSalary("Terisa", 300000.00);
+		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terisa");
+		Assert.assertTrue(result);
+	}
+
+	// UC5
 	@Test
 	public void givenEmployeePayrollDataWhenRetrievedBasedOnStartDateShouldReturnProperResult()
 			throws DatabaseConnectionException {
@@ -43,6 +65,7 @@ public class EmployeePayrollServiceTest {
 		Assert.assertEquals(matchingRecords.get(0), employeePayrollService.getEmployeePayrollData("Terisa"));
 	}
 
+	// UC6
 	@Test
 
 	public void givenEmployeePerformed_VariousOperations_ShouldGiveProperResult() throws DatabaseConnectionException {
@@ -52,11 +75,12 @@ public class EmployeePayrollServiceTest {
 		Assert.assertEquals(30000000.0, averageSalaryByGender.get("F"), 0.0);
 	}
 
+	// UC7 and UC8
 	@Test
 	public void givenNewEmployee_WhenAdded_ShouldGiveProperResult() throws DatabaseConnectionException {
 		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 		employeePayrollService.readEmployeePayrollData();
-		employeePayrollService.addEmployeeToPayroll("Mark", 500000.00, "M", LocalDate.now());
+		employeePayrollService.addEmployeeToPayroll("Mark", 5000000.00, "M", LocalDate.now());
 		boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Mark");
 		Assert.assertTrue(result);
 	}
