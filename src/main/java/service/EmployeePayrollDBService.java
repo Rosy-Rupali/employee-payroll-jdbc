2ff8488022971a4e74156be88d96b709450db318
Rosy-Rupali <itrosyrupali4037@gmail.com>
2021-07-12 06:34:06 +0530

    refactored : to implement the transaction

--- src/main/java/service/EmployeePayrollDBService.java
+++ src/main/java/service/EmployeePayrollDBService.java
@@ -100,7 +100,7 @@ public class EmployeePayrollDBService {
 				double salary = resultSet.getDouble("salary");
 				String gender = resultSet.getString("gender");
 				LocalDate start = resultSet.getDate("start").toLocalDate();
-				employeePayrollList.add(new EmployeePayrollData(id, objectname, salary,  gender, start));
+				employeePayrollList.add(new EmployeePayrollData(id, objectname, salary, gender, start));
 			}
 			return employeePayrollList;
 		} catch (SQLException e) {
@@ -152,8 +152,8 @@ public class EmployeePayrollDBService {
 	}
 
 	/**
-	 * prepareStatementForEmployeePayroll method for single query to get the data from
-	 * database added try and catch block to throw sql exception
+	 * prepareStatementForEmployeePayroll method for single query to get the data
+	 * from database added try and catch block to throw sql exception
 	 * 
 	 * @throws DatabaseConnectionException
 	 */
@@ -190,8 +190,8 @@ public class EmployeePayrollDBService {
 	}
 
 	/**
-	 * prepareStatementForEmployeePayrollDataRetrival method for single query to get the data from
-	 * database added try and catch block to throw sql exception
+	 * prepareStatementForEmployeePayrollDataRetrival method for single query to get
+	 * the data from database added try and catch block to throw sql exception
 	 * 
 	 * @throws DatabaseConnectionException
 	 */
@@ -204,16 +204,19 @@ public class EmployeePayrollDBService {
 			throw new DatabaseConnectionException("Unable to create prepare statement");
 		}
 	}
-	
+
 	/**
 	 * created method to retrieve data from data base in a particular date range
+	 * 
 	 * @param startDate in LocalDate format
-	 * @param endDate in LocalDate format
+	 * @param endDate   in LocalDate format
 	 * @return this.getEmployeePayrollListFromResultset(resultSet)
 	 * @throws DatabaseConnectionException
 	 */
-	public List<EmployeePayrollData> getEmployeePayrollDataByStartingDate(LocalDate startDate, LocalDate endDate) throws DatabaseConnectionException {
-		String sql = String.format("select * from employee_payroll where start between cast('%s' as date) and cast('%s' as date);",
+	public List<EmployeePayrollData> getEmployeePayrollDataByStartingDate(LocalDate startDate, LocalDate endDate)
+			throws DatabaseConnectionException {
+		String sql = String.format(
+				"select * from employee_payroll where start between cast('%s' as date) and cast('%s' as date);",
 				startDate.toString(), endDate.toString());
 		try (Connection connection = this.getConnection()) {
 			Statement statement = connection.createStatement();
@@ -223,79 +226,104 @@ public class EmployeePayrollDBService {
 			throw new DatabaseConnectionException("Connection Failed.");
 		}
 	}
-	
+
 	/**
-	 * created performVariousOperation method to get average, min, max, count and sum of salary
-	 * group by gender data from database
-     * by using mysql query in the method and mapped gender and  average, min, max, count and sum of Salary
-	 * @param column : salary
-	 * @param operation :  average, min, max, count and sum 
+	 * created performVariousOperation method to get average, min, max, count and
+	 * sum of salary group by gender data from database by using mysql query in the
+	 * method and mapped gender and average, min, max, count and sum of Salary
+	 * 
+	 * @param column    : salary
+	 * @param operation : average, min, max, count and sum
 	 * @return the salary used by various operations mapped with gender
 	 * @throws DatabaseConnectionException
 	 */
-	public Map<String, Double> performVariousOperations(String column, String operation) throws DatabaseConnectionException{
-		String sql=String.format("select gender , %s(%s) from employee_payroll group by gender;" , operation , column);
-		Map<String,Double> mapValues = new HashMap<>();
-		try(Connection connection = this.getConnection()) {
+	public Map<String, Double> performVariousOperations(String column, String operation)
+			throws DatabaseConnectionException {
+		String sql = String.format("select gender , %s(%s) from employee_payroll group by gender;", operation, column);
+		Map<String, Double> mapValues = new HashMap<>();
+		try (Connection connection = this.getConnection()) {
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 			ResultSet resultSet = preparedStatement.executeQuery();
-			while(resultSet.next())
-			{
+			while (resultSet.next()) {
 				mapValues.put(resultSet.getString(1), resultSet.getDouble(2));
 			}
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			throw new DatabaseConnectionException("Connection Failed.");
 		}
 		return mapValues;
 	}
-	
+
 	/**
-	 * added new employee to database
-	 * @param name : first argument of the method
-	 * @param salary : second argument of the method
+	 * added new employee and payroll details to the table in database
+	 * 
+	 * @param name      : first argument of the method
+	 * @param salary    : second argument of the method
 	 * @param startdate : fourth argument of the method
-	 * @param gender : third argument of the method
+	 * @param gender    : third argument of the method
 	 * @return employeePayrollData
 	 * @throws DatabaseConnectionException
 	 */
-	public EmployeePayrollData addEmployeeToPayroll(String name, double salary, LocalDate startdate, String gender) throws DatabaseConnectionException {
-		int employeeId=-1;
-		Connection connection=null;
-		EmployeePayrollData employeePayrollData=null;
-		try
-		{
-			connection=this.getConnection();
-		}
-		catch (Exception e) {
+	public EmployeePayrollData addEmployeeToPayroll(String name, double salary, LocalDate startdate, String gender)
+			throws DatabaseConnectionException {
+		int employeeId = -1;
+		Connection connection = null;
+		EmployeePayrollData employeePayrollData = null;
+		try {
+			connection = this.getConnection();
+			connection.setAutoCommit(false);
+		} catch (Exception e) {
 			throw new DatabaseConnectionException("Error");
 		}
-		try(Statement statement=connection.createStatement())
-		{
-		String sql=String.format("INSERT INTO employee_payroll (name,gender,salary,start) VALUES ('%s','%s','%s','%s')",name,gender,salary,startdate);
-			int rowsAffected=statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
-			if(rowsAffected==1)
-			{
-				ResultSet resultSet=statement.getGeneratedKeys();
-				if(resultSet.next()) employeeId=resultSet.getInt(1);
+		try (Statement statement = connection.createStatement()) {
+			String sql = String.format(
+					"INSERT INTO employee_payroll (name,gender,salary,start) VALUES ('%s','%s','%s','%s')", name,
+					gender, salary, startdate);
+			int rowsAffected = statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+			if (rowsAffected == 1) {
+				ResultSet resultSet = statement.getGeneratedKeys();
+				if (resultSet.next())
+					employeeId = resultSet.getInt(1);
 			}
-			employeePayrollData=new EmployeePayrollData(employeeId, name, salary, gender, startdate);
-		}
-		catch (SQLException e) {
-			throw new DatabaseConnectionException("Could Not Add");}
-		try(Statement statement=connection.createStatement())
-		{
-			double deductions=salary*0.2;
-			double taxablePay=salary-deductions;
-			double tax=taxablePay*0.1;
-			double netPay=salary-tax;
-			String sql=String.format("INSERT INTO payroll_details (emp_id,basic_pay,deductions,taxable_pay,tax,net_pay) VALUES "
-					+ "('%s','%s','%s','%s','%s','%s')",employeeId,salary,deductions,taxablePay,tax,netPay);
-			int rowsAffected=statement.executeUpdate(sql);
-			if(rowsAffected==1)
-				employeePayrollData=new EmployeePayrollData(employeeId, name, salary, gender, startdate);
+			employeePayrollData = new EmployeePayrollData(employeeId, name, salary, gender, startdate);
 		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+			throw new DatabaseConnectionException("Could Not Add");
+		}
+		try (Statement statement = connection.createStatement()) {
+			double deductions = salary * 0.2;
+			double taxablePay = salary - deductions;
+			double tax = taxablePay * 0.1;
+			double netPay = salary - tax;
+			String sql = String.format(
+					"INSERT INTO payroll_details (employeeId,salary,deductions,taxable_pay,tax,net_pay) VALUES "
+							+ "('%s','%s','%s','%s','%s','%s')",
+					employeeId, salary, deductions, taxablePay, tax, netPay);
+			int rowsAffected = statement.executeUpdate(sql);
+			if (rowsAffected == 1)
+				employeePayrollData = new EmployeePayrollData(employeeId, name, salary, gender, startdate);
+		} catch (SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 			throw new DatabaseConnectionException("Not Able to add");
+		}
+		try {
+			connection.commit();
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		} finally {
+			if (connection != null)
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
 		}
 		return employeePayrollData;
 	}
